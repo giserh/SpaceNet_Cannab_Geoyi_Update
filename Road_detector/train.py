222379a3d23ea05a988cd81741903940e68e173d
Zhuangfang Yi 依庄防 <geospatialanalystyi@gmail.com>
2018-05-30 18:04:13 -0400

    band index correction

--- Road_detector/train.py
+++ Road_detector/train.py
@@ -26,7 +26,7 @@ input_shape = (320, 320)
 # city_datasets = dict(Vegas = 'AOI_2_Vegas_Roads_Train',
 #                      Paris = 'AOI_3_Paris_Roads_Train')
 # city_id = -1
-batch_size = 64
+batch_size = 16
 # it = -1
 all_files, all_masks = datafiles()
 # fold_nums = [0, 1]
